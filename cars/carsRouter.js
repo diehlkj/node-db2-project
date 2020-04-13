09b43abf104ebfc4277342cf51b197a923fa93ce
Ken Diehl <diehlkj.99@gmail.com>
2020-04-12 21:52:47 -0400

    Completed MVP, seed stretch, and endpoints stretch. All endpoints tested and functional.

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -3,19 +3,37 @@ const Cars = require('./carsDb.js');
 const router = express.Router();
 
 router.get('/', (req, res)=> {
-    Cars.get()
-        .then(data => {
-            res.status(200).json({ MESSAGE: 'Your [GET] request was successful:', data });
-        })
-        .catch(err => {
-            res.status(500).json({ MESSAGE: 'There was an error with your [GET] request:', err });
-        });
+    const { limit, sortBy, sortDir } = req.body;
+    !sortBy
+    ? (
+        Cars.get(limit, 'id', sortDir)
+            .then(vehicleData => {
+                res.status(200).json({ MESSAGE: 'Your [GET] request was successful:', vehicleData });
+            })
+            .catch(err => {
+                res.status(500).json({ MESSAGE: 'There was an error with your [GET] request:', err });
+            })
+    ) : (
+        Cars.get(limit, sortBy, sortDir)
+            .then(vehicleData => {
+                res.status(200).json({ MESSAGE: 'Your [GET] request was successful:', vehicleData });
+            })
+            .catch(err => {
+                res.status(500).json({ MESSAGE: 'There was an error with your [GET] request:', err });
+            })
+    );
 });
 
 router.get('/:id', (req, res)=> {
-    Cars.getById()
-        .then(data => {
-            res.status(200).json({ MESSAGE: 'Your [GET] request was successful:', data });
+    const { id } =  req.params;
+    Cars.getById(id)
+        .then(vehicleData => {
+            vehicleData.length === 0
+            ? (
+                res.status(404).json({ MESSAGE: 'The record you requested could not be located.'})
+            ) : (
+                res.status(200).json({ MESSAGE: 'Your [GET] request was successful:', vehicleData })
+            );
         })
         .catch(err => {
             res.status(500).json({ MESSAGE: 'There was an error with your [GET] request:', err });
@@ -23,11 +41,38 @@ router.get('/:id', (req, res)=> {
 });
 
 router.post('/', (req, res)=> {
-    Cars.insert()
-        .then(data => {
-            res.status(201).json({ MESSAGE: 'Your [POST] request was successful:', data });
+    const newVehicle = req.body;
+    Cars.insert(newVehicle)
+        .then(vehicleData => {
+            res.status(201).json({ MESSAGE: 'Your [POST] request was successful:', vehicleData });
         })
         .catch(err => {
             res.status(500).json({ MESSAGE: 'There was an error with your [POST] request:', err });
         });
 });
+
+router.put('/:id', (req, res)=> {
+    const { id } =  req.params;
+    const changes = req.body;
+    
+    Cars.update(id, changes)
+        .then(vehicleData => {
+            res.status(201).json({ MESSAGE: 'Your [PUT] request was successful:', vehicleData });
+        })
+        .catch(err => {
+            res.status(500).json({ MESSAGE: 'There was an error with your [PUT] request:', err });
+        });
+});
+
+router.delete('/:id', (req, res)=> {
+    const { id } =  req.params;
+    Cars.remove(id)
+        .then(vehicleData => {
+            res.status(201).json({ MESSAGE: 'Your [DELETE] request was successful:', VHEHICLE_REMOVED: id });
+        })
+        .catch(err => {
+            res.status(500).json({ MESSAGE: 'There was an error with your [DELETE] request:', err });
+        });
+});
+
+module.exports = router;
